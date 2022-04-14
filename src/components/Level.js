ec532cbe41a374713a73d301be218db60ab806ba
ericchi00 <ericchi00@gmail.com>
2022-04-14 14:00:38 -0700

    added timer & coord win condition

--- src/components/Level.js
+++ src/components/Level.js
@@ -1,24 +1,52 @@
 import React, { useEffect, useState } from 'react';
 
-const Level = ({ img }) => {
+const Level = ({ img, coordsStart, coordsEnd }) => {
 	const [startGame, setStartGame] = useState(false);
 	const [win, setWin] = useState(false);
-	const [timer, setTimer] = useState();
+	const [running, setRunning] = useState(false);
+	const [time, setTime] = useState(0);
+
+	useEffect(() => {
+		let interval = null;
+		if (running) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime + 10);
+			}, 10);
+		} else {
+			clearInterval(interval);
+		}
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [running]);
 
 	const pictureHandler = (e) => {
 		const x = e.nativeEvent.offsetX;
 		const y = e.nativeEvent.offsetY;
-		if (x === y) {
-			setWin(true);
+		if (
+			x > coordsStart[0] &&
+			x < coordsStart[1] &&
+			y > coordsEnd[0] &&
+			y < coordsEnd[1]
+		) {
+			console.log('you win');
 		}
 	};
 
 	const startGameHandler = (e) => {
 		e.preventDefault();
+		setRunning(true);
 		setStartGame(true);
 	};
 	return (
 		<div className="waldo-img-wrapper">
+			<div className="timer">
+				<span>{('0' + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
+				<span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+				<span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+				{/* <span>{('0' + ((time / 10) % 100)).slice(-2)}</span> */}
+			</div>
 			{startGame ? (
 				<>
 					<img src={img} alt="Where's Waldo" onClick={pictureHandler} />
@@ -37,7 +65,8 @@ const Level = ({ img }) => {
 				<div className="start-game">
 					<p>
 						In order to win, you'll need to click on Waldo. Timer is started as
-						soon as you press start.
+						soon as you press start. There is always one Waldo with glasses in
+						each level.
 					</p>
 					<button
 						type="button"
