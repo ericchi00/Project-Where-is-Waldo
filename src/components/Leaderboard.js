dd932911260771b3dc40296ba2116f5eb6929d10
ericchi00 <ericchi00@gmail.com>
2022-04-14 17:55:01 -0700

    grab data from database

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import {
+	collection,
+	getFirestore,
+	orderBy,
+	query,
+	onSnapshot,
+	getDoc,
+	getDocs,
+} from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 
 const Leaderboard = (props) => {
-    return (
-        // return map function of array from backend firebase
-    )
+	const [level1, setLevel1] = useState([]);
+	const [level2, setLevel2] = useState([]);
+	const [level3, setLevel3] = useState([]);
+
+	useEffect(() => {
+		const first = async () => {
+			const waldo1 = collection(getFirestore(), 'Waldo 1');
+			const level1Query = query(waldo1, orderBy('time'));
+			return await getDocs(level1Query);
+		};
+
+		first().then((data) => {
+			data.forEach((item) => {
+				console.log(item._document.data.value.mapValue.fields);
+			});
+		});
+	});
+
+	return <div></div>;
 };
+
+export default Leaderboard;
