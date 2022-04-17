f242f57a69afcf3f65c3d7557a4ac5a280329438
ericchi00 <ericchi00@gmail.com>
2022-04-16 20:29:23 -0700

    fixed leaderboard  promises & styling

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,8 +3,6 @@ import {
 	getFirestore,
 	orderBy,
 	query,
-	onSnapshot,
-	getDoc,
 	getDocs,
 } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
@@ -15,20 +13,77 @@ const Leaderboard = (props) => {
 	const [level3, setLevel3] = useState([]);
 
 	useEffect(() => {
-		const first = async () => {
-			const waldo1 = collection(getFirestore(), 'Waldo 1');
-			const level1Query = query(waldo1, orderBy('time'));
-			return await getDocs(level1Query);
+		let level1leaderboard = [];
+		let level2leaderboard = [];
+		let level3leaderboard = [];
+
+		const querySearch = async (levelName) => {
+			const waldo = collection(getFirestore(), levelName);
+			const search = query(waldo, orderBy('time'));
+			return await getDocs(search);
 		};
 
-		first().then((data) => {
-			data.forEach((item) => {
-				console.log(item._document.data.value.mapValue.fields);
+		const allLevelQuery = async () => {
+			const firstResult = await querySearch('Waldo 1').then((data) => {
+				data.forEach((item) => {
+					level1leaderboard.push(item._document.data.value.mapValue.fields);
+				});
 			});
-		});
-	});
+			const secondResult = await querySearch('Waldo 2').then((data) => {
+				data.forEach((item) => {
+					level2leaderboard.push(item._document.data.value.mapValue.fields);
+				});
+			});
+			const thirdResult = await querySearch('Waldo 3').then((data) => {
+				data.forEach((item) => {
+					level3leaderboard.push(item._document.data.value.mapValue.fields);
+				});
+			});
+			setLevel1(level1leaderboard);
+			setLevel2(level2leaderboard);
+			setLevel3(level3leaderboard);
+		};
 
-	return <div></div>;
+		allLevelQuery();
+	}, []);
+
+	return (
+		<div className="leaderboard">
+			<ul className="first-leaderboard">
+				<h3>Level 1 Leaderboard</h3>
+				{level1.map((item, i) => {
+					return (
+						<li key={i}>
+							<h4>{item.name.stringValue}</h4>
+							<h4>{item.time.integerValue  / 1000}s</h4>
+						</li>
+					);
+				})}
+			</ul>
+			<ul className="second-leaderboard">
+				<h3>Level 2 Leaderboard</h3>
+				{level2.map((item, i) => {
+					return (
+						<li key={i}>
+							<h4>{item.name.stringValue}</h4>
+							<h4>{item.time.integerValue / 1000}s</h4>
+						</li>
+					);
+				})}
+			</ul>
+			<ul className="third-leaderboard">
+				<h3>Level 3 Leaderboard</h3>
+				{level3.map((item, i) => {
+					return (
+						<li key={i}>
+							<h4>{item.name.stringValue}</h4>
+							<h4>{item.time.integerValue / 1000}s</h4>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default Leaderboard;
